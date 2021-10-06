4348cdbcb04cb9e76c624677be38f736af56c404
João Felix <jvbfelix@gmail.com>
2021-10-06 15:31:25 -0300

    fix vuex config

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export default createStore({
     },
     logout: (state) => {
       localStorage.removeItem('CobliAPIKey')
-      state.GestorKey = localStorage.getItem('CobliAPIKey') || ''
+      state.ApiKey = localStorage.getItem('CobliAPIKey') || ''
     },
   },
   actions: {
@@ -23,7 +23,7 @@ export default createStore({
     },
   },
   getters: {
-    isAuthenticated: state => !!state.GestorKey,
+    isAuthenticated: state => !!state.ApiKey,
     ApiKey: state => state.ApiKey,
   },
   modules: {

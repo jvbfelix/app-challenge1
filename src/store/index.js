8bca48bad1380c69ccf22ede6d8b5549a47902ed
João Felix <jvbfelix@gmail.com>
2021-10-06 15:30:39 -0300

    add vuex config

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    ApiKey: localStorage.getItem('CobliAPIKey') || '',
   },
   mutations: {
+    login: (state,t) => { 
+      localStorage.setItem('CobliAPIKey', t)
+      state.ApiKey = localStorage.getItem('CobliAPIKey') || ''
+    },
+    logout: (state) => {
+      localStorage.removeItem('CobliAPIKey')
+      state.GestorKey = localStorage.getItem('CobliAPIKey') || ''
+    },
   },
   actions: {
+    login (state,t) {
+      this.commit('login',t)
+    },
+    logout () {
+      this.commit('logout')
+    },
+  },
+  getters: {
+    isAuthenticated: state => !!state.GestorKey,
+    ApiKey: state => state.ApiKey,
   },
   modules: {
   }

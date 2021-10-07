735f8a052da8b56656c7956462e4b2aee382f101
João Felix <jvbfelix@gmail.com>
2021-10-07 14:14:44 -0300

    dashboard static view

---
+++ src/components/DashNumber.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Number">
+    <h3>{{ num }}</h3>
+    <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashNumber',
+  props: {
+    num: String,
+    name: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.Number{
+    padding: 20px;
+    border-radius: 7px;
+    margin: 15px;
+    background: linear-gradient(30deg, #CACACA, #317AF6);
+    color: #FFFFFF;
+    box-shadow: 0 0 10px #0000002A;
+    h3 {
+        font-weight: 600;
+        margin: 10px 0 0;
+        text-align: left;
+    }
+}
+</style>
--- src/views/Dash.vue
+++ src/views/Dash.vue
@@ -1,5 +1,109 @@
 <template>
   <div class="dash">
-    <h1>This is the dash page</h1>
+    <div class="numbers">
+      <DashNumber num="20" name="manutenção"/>
+      <DashNumber num="30" name="manutenção"/>
+      <DashNumber num="40" name="manutenção"/>
+      <DashNumber num="50" name="manutenção"/>
+    </div>
+    <div class="graphs">
+      <apexchart type="radialBar" height="350" :options="chartOptions" :series="series"></apexchart>
+      <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+      <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+      <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+    </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import DashNumber from '@/components/DashNumber.vue'
+import store from '../store'
+//import axios from 'axios';
+import { computed } from '@vue/reactivity'
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+  name: 'Home',
+  components: {
+    DashNumber,
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      APIkey: '',
+       series: [44, 55, 67, 83],
+          chartOptions: {
+            chart: {
+              height: 350,
+              type: 'radialBar',
+            },
+            plotOptions: {
+              radialBar: {
+                dataLabels: {
+                  name: {
+                    fontSize: '22px',
+                  },
+                  value: {
+                    fontSize: '16px',
+                  },
+                  total: {
+                    show: true,
+                    label: 'Total',
+                    formatter: function () {
+                      // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+                      return 249
+                    }
+                  }
+                }
+              }
+            },
+            labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+          },
+    }
+  },
+  setup(props) {
+    const isAuthenticated = computed(() => store.getters.isAuthenticated)
+    console.log(props.img)
+    return {
+      isAuthenticated
+    }
+  },
+  mounted() {
+    if(!this.isAuthenticated) {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.dash {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+}
+
+.numbers {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  flex-wrap: wrap;
+}
+
+.graphs {
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  flex-wrap: wrap;
+
+  .vue-apexcharts {
+    margin: 30px;
+  }
+}
+
+</style>
